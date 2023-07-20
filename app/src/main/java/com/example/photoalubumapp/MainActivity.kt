a48eda944b0d95e9eb210c43b4434266d8a3aa37
Kriminalac <ibrahimtanicho@gmail.com>
2023-07-20 10:57:47 +0200

    styling App and implimenting app functionality

--- app/src/main/java/com/example/photoalubumapp/MainActivity.kt
+++ app/src/main/java/com/example/photoalubumapp/MainActivity.kt
@@ -9,12 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,8 +27,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.photoalubumapp.ui.theme.PhotoAlubumAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,27 +58,102 @@ fun NextPic(){
     when (currentPic) {
         1 -> {
             PhotoAlbum(
-                drawableResourceId = R.drawable.lemon_tree,
-                title = R.string.lemon_tree,
-                year = R.string.lemon_tree_2015_davinci,
-                nextClick = { currentPic++ }
-            ) { currentPic = 9 }
+                contentDescription = R.string.mona_lisa_description.toString(),
+                drawableResourceId = R.drawable.mona_lisa,
+                title = R.string.mona_lisa_title,
+                author = R.string.mona_lisa_painter,
+                year = R.string.mona_lisa_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic = 1}
+            )
         }
         2-> {
             PhotoAlbum(
-                drawableResourceId = R.drawable.lemon_squeeze,
-                title = R.string.squeezed_lemon,
-                year = R.string.squeezed_lemon_2018_vangoch,
-                nextClick = { currentPic++ }
-            ) { currentPic = 1 }
+                contentDescription = R.string.the_girl_with_pearl_description.toString(),
+                drawableResourceId = R.drawable.the_girl_with_a_pearl_parring,
+                title = R.string.the_girl_with_pearl_title,
+                author = R.string.the_girl_with_pearl_description,
+                year = R.string.the_girl_with_pearl_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic--}
+            )
         }
         3-> {
             PhotoAlbum(
-                drawableResourceId = R.drawable.lemon_tree,
-                title = R.string.squeezed_lemon,
-                year = R.string.squeezed_lemon_2018_vangoch,
-                nextClick = { currentPic++ }
-            ) { currentPic-- }
+                contentDescription = R.string.guernica_description.toString(),
+                drawableResourceId = R.drawable.guernica,
+                title = R.string.guernica_title,
+                author = R.string.guernica_painter,
+                year = R.string.guernica_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic--}
+            )
+        }
+        4-> {
+            PhotoAlbum(
+                contentDescription = R.string.persistence_of_memory_description.toString(),
+                drawableResourceId = R.drawable.persistenceofmemory,
+                title = R.string.persistence_of_memory_title,
+                author = R.string.persistence_of_memory_painter,
+                year = R.string.persistence_of_memory_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic-- }
+            )
+            }
+        5-> {
+            PhotoAlbum(
+                contentDescription = R.string.the_birth_of_venus_description.toString(),
+                drawableResourceId = R.drawable.sandro_botticelli,
+                title = R.string.the_birth_of_venus_title,
+                author = R.string.the_birth_of_venus_painter,
+                year = R.string.the_birth_of_venus_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic-- }
+            )
+        }
+        6-> {
+            PhotoAlbum(
+                contentDescription = R.string.the_last_supper_description.toString(),
+                drawableResourceId = R.drawable.the_last_supper,
+                title = R.string.the_last_supper_title,
+                author = R.string.the_last_supper_painter,
+                year = R.string.the_last_supper_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic-- }
+            )
+        }
+        7-> {
+            PhotoAlbum(
+                contentDescription = R.string.the_night_watch_description.toString(),
+                drawableResourceId = R.drawable.the_night_watch,
+                title = R.string.the_night_watch_title,
+                author = R.string.the_night_watch_painter,
+                year = R.string.the_night_watch_year,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic-- }
+            )
+        }
+        8-> {
+            PhotoAlbum(
+                contentDescription = R.string.whistlers_mothers_description.toString(),
+                drawableResourceId = R.drawable.whistlers_mother_high_res,
+                title = R.string.whistlers_mothers_title,
+                year = R.string.whistlers_mothers_year,
+                author = R.string.whistlers_mothers_painter,
+                nextClick = { currentPic++ },
+                prevClick = { currentPic-- }
+            )
+        }
+        9-> {
+            PhotoAlbum(
+                contentDescription = R.string.starry_night_description.toString(),
+                drawableResourceId = R.drawable.starry_night,
+                title = R.string.starry_night_title,
+                year = R.string.starry_night_year,
+                author = R.string.starry_night_painter,
+                nextClick = { currentPic = 9},
+                prevClick = { currentPic-- }
+            )
         }
     }
 }
@@ -83,40 +162,52 @@ fun NextPic(){
 fun PhotoAlbum(
     drawableResourceId: Int,
     title: Int,
+    author: Int,
     year: Int,
+    contentDescription: String,
     nextClick: () -> Unit,
     prevClick: () -> Unit
 ){
 
     Column(
-        modifier = Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
         Spacer(modifier= Modifier.height(40.dp))
 
         Image(
             painter = painterResource(drawableResourceId),
-            contentDescription = null,modifier = Modifier
+            contentDescription = contentDescription,modifier = Modifier
+                .size(400.dp)
                 .padding(20.dp)
-                .fillMaxWidth()
         )
         Text(
                 text = stringResource(title),
-                modifier = Modifier.padding(12.dp),color = colorScheme.primary)
+                textAlign= TextAlign.Center,
+                color = MaterialTheme.colorScheme.primary,
+                fontWeight = FontWeight.Normal,
+                fontSize = 20.sp,
+                modifier = Modifier.padding(20.dp))
         Text(
-                text = stringResource(year),
-                modifier = Modifier.padding(12.dp),color = colorScheme.primary)
-        Row{
+                text = (stringResource(author) + stringResource(year)),
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier.padding(12.dp))
+        Row(
+            modifier = Modifier
+        ){
             Button(
-                onClick = {nextClick()},
-            modifier = Modifier.padding(20.dp)) {
-                Text(
-                    text = stringResource(R.string.next_photo))
-            }
-            Button(
-                onClick =  {prevClick()},
+                onClick = {prevClick()},
                 modifier = Modifier.padding(20.dp)) {
                 Text(
-                    text= stringResource(R.string.previous_photo))
+                    text = stringResource(R.string.previous_photo))
+            }
+            Button(
+                onClick =  {nextClick()},
+                modifier = Modifier.padding(20.dp)) {
+                Text(
+                    text= stringResource(R.string.next_photo))
              }
             }
         }
